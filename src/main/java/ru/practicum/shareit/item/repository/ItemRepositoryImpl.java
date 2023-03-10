d305bb18ad7a5f62672fdc95d62df1940d93909e
MaricTheirin <ghostmeee@yandex.ru>
2023-03-10 14:25:19 +0300

    feat: Реализовать интерфейс ItemRepository

---
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -0,0 +1,39 @@
+package ru.practicum.shareit.item.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.item.model.Item;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ItemRepositoryImpl implements ItemRepository {
+
+    private final Map<Long, Map<Long, Item>> items = new HashMap<>();
+
+    @Override
+    public Item saveItem(Item item) {
+        long ownerId = item.getOwnerId();
+        if (!items.containsKey(ownerId)) {
+            items.put(ownerId, new HashMap<>());
+        }
+
+        items.get(ownerId).put(item.getItemId(), item);
+        return item;
+    }
+
+    @Override
+    public Item getItem(Long userId, Long itemId) {
+        return items.get(userId).get(itemId);
+    }
+
+    @Override
+    public Item updateItem(Item item) {
+        items.get(item.getOwnerId()).put(item.getItemId(), item);
+        return item;
+    }
+
+    @Override
+    public Item deleteItem(Long userId, Long itemId) {
+        return items.get(userId).remove(itemId);
+    }
+}
