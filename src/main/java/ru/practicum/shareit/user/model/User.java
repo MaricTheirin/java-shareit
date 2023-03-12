5c745270061402876dfc87968bd7212c0b1479f6
MaricTheirin <ghostmeee@yandex.ru>
2023-03-12 18:00:51 +0300

    feat: Добавить метод получения всех пользователей, добавить проверку полей

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,12 +1,24 @@
 package ru.practicum.shareit.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class User {
 
-    private final long userId;
+    private long id;
+    @NotEmpty
     private final String name;
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotBlank
+    @NotNull
     private final String email;
 
 }
