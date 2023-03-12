2990739ce24ae056f6abc6202ac2e2f21c4eb172
MaricTheirin <ghostmeee@yandex.ru>
2023-03-12 18:41:41 +0300

    chore: Обновить UserDto/UserDtoMapper

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,11 +1,14 @@
 package ru.practicum.shareit.user.dto;
 
 import lombok.Data;
+import javax.validation.constraints.Email;
 
 @Data
 public class UserDto {
 
+    private final Long id;
     private final String name;
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private final String email;
 
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserDtoMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDtoMapper.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.user.dto;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.model.User;
 
+@Slf4j
 @Component
 public class UserDtoMapper {
 
@@ -11,11 +13,15 @@ public class UserDtoMapper {
     }
 
     public User mapDtoToUser(UserDto userDto) {
-        return new User(0, userDto.getName(), userDto.getEmail());
+        User user = new User(0, userDto.getName(), userDto.getEmail());
+        log.trace("Выполнено преобразование из {} в {}", userDto, user);
+        return user;
     }
 
     public UserDto mapUserToDto(User user) {
-        return new UserDto(user.getName(), user.getEmail());
+        UserDto userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        log.trace("Выполнено преобразование из {} в {}", user, userDto);
+        return userDto;
     }
 
 }
