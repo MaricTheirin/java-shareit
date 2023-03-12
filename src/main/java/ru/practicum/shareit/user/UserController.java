67941a138f0f1050b62ac6b328a58b8bdb514d99
MaricTheirin <ghostmeee@yandex.ru>
2023-03-12 18:00:46 +0300

    feat: Добавить метод получения всех пользователей, добавить проверку полей

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -17,7 +19,7 @@ public class UserController {
     }
 
     @PostMapping
-    public UserDto createUser (UserDto userDto) {
+    public UserDto createUser (@Valid @RequestBody UserDto userDto) {
         return userService.createUser(userDto);
     }
 
@@ -27,13 +29,18 @@ public class UserController {
     }
 
     @PatchMapping("/{userId}")
-    public UserDto updateUser(@PathVariable Long userId, UserDto userDto) {
+    public UserDto updateUser(@PathVariable Long userId, @Valid @RequestBody UserDto userDto) {
         return userService.updateUser(userId, userDto);
     }
 
     @DeleteMapping("/{userId}")
     public UserDto deleteUser(@PathVariable Long userId) {
         return userService.deleteUser(userId);
+    }
+
+    @GetMapping()
+    public List<UserDto> readAllUsers() {
+        return userService.readAllUsers();
     }
 
 }
