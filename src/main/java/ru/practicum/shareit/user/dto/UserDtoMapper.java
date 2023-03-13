e3642749833b10e19f5a0ff9da9bb9b24eae2d86
MaricTheirin <ghostmeee@yandex.ru>
2023-03-13 12:24:45 +0300

    chore: Мелкие правки для соответствия checkstyle

--- src/main/java/ru/practicum/shareit/user/dto/UserDtoMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDtoMapper.java
@@ -8,7 +8,8 @@ import ru.practicum.shareit.user.model.User;
 @Component
 public class UserDtoMapper {
 
-    private final String MESSAGE = "Выполнено преобразование объекта из {} в {}";
+    //В этом месте должна быть константа private static final String DEFAULT_MESSAGE, но checkstyle её не пропускает
+    private final String defaultMessage = "Выполнено преобразование объекта из {} в {}";
 
     public User mapDtoToUser(Long userId, UserDto userDto) {
         return new User(userId, userDto.getName(), userDto.getEmail());
@@ -16,13 +17,13 @@ public class UserDtoMapper {
 
     public User mapDtoToUser(UserDto userDto) {
         User user = new User(0, userDto.getName(), userDto.getEmail());
-        log.trace(MESSAGE, userDto, user);
+        log.trace(defaultMessage, userDto, user);
         return user;
     }
 
     public UserDto mapUserToDto(User user) {
         UserDto userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
-        log.trace(MESSAGE, user, userDto);
+        log.trace(defaultMessage, user, userDto);
         return userDto;
     }
 
