990be59ca1af390af54aecd05507bab2fde66bf5
MaricTheirin <ghostmeee@yandex.ru>
2023-03-18 22:44:06 +0300

    feat: Убрать валидацию User, т.к. она выполняется в UserDto

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -3,10 +3,6 @@ package ru.practicum.shareit.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -14,11 +10,7 @@ import javax.validation.constraints.NotNull;
 public class User {
 
     private long id;
-    @NotEmpty
-    private final String name;
-    @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
-    @NotBlank
-    @NotNull
-    private final String email;
+    private String name;
+    private String email;
 
 }
