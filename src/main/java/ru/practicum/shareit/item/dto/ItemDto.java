7b267bdf2a385c6042ec09b906ab8aabd12cd834
MaricTheirin <ghostmeee@yandex.ru>
2023-03-27 09:09:28 +0300

    feat: Добавить вывод предыдущего и следующего бронирования при выводе информации о предмете

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.service.validation.Create;
 import ru.practicum.shareit.service.validation.Update;
 import javax.validation.constraints.NotBlank;
@@ -11,15 +12,19 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class ItemDto {
 
-    private long id;
+    private final long id;
 
     @NotBlank(groups = Create.class, message = "Невозможно сохранить предмет с пустым наименованием")
-    private String name;
+    private final String name;
 
     @NotBlank(groups = {Create.class, Update.class}, message = "Невозможно сохранить предмет с пустым описанием")
-    private String description;
+    private final String description;
 
     @NotNull(groups = Create.class, message = "Невозможно сохранить предмет без указания его доступности")
-    private Boolean available;
+    private final Boolean available;
+
+    private BookingDto lastBooking;
+
+    private BookingDto nextBooking;
 
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoMapper.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.item.model.Item;
 
 @Slf4j
@@ -9,14 +11,30 @@ import ru.practicum.shareit.item.model.Item;
 public class ItemDtoMapper {
 
     private static final String OBJECT_MAPPED_MESSAGE = "Выполнено преобразование объекта из {} в {}";
+    private final BookingRepository bookingRepository;
+
+    @Autowired
+    public ItemDtoMapper(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
 
     public ItemDto mapItemToDto(Item item) {
+        return mapItemToDto(item, false);
+    }
+
+    public ItemDto mapItemToDto(Item item, boolean belongsToRequestedUser) {
         ItemDto mappedDto = new ItemDto(
                 item.getId(),
                 item.getName(),
                 item.getDescription(),
-                item.isAvailable()
+                item.isAvailable(),
+                null,
+                null
         );
+        if (belongsToRequestedUser) {
+            mappedDto.setLastBooking(bookingRepository.getLastBooking(item.getId()));
+            mappedDto.setNextBooking(bookingRepository.getNextBooking(item.getId()));
+        }
         log.trace(OBJECT_MAPPED_MESSAGE, item, mappedDto);
         return mappedDto;
     }
