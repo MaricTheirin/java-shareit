ecb04ac1679da8fe238470eea46fdb0f1927d329
MaricTheirin <ghostmeee@yandex.ru>
2023-03-27 14:37:46 +0300

    chore: Добавить конструкторы

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -1,12 +1,15 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "comments", schema = "public")
 public class Comment {
