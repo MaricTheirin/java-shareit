cb148b6f3108ced70b96c807d095ccca31a18b98
MaricTheirin <ghostmeee@yandex.ru>
2023-03-27 09:13:04 +0300

    feat: Добавить BookingRepository с использованием JPQL/Query Creation/Кастомными методами

---
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.booking.repository;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.model.Item;
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long>, BookingRepositoryCustom {
+
+    List<Booking> findAllByBookerIdOrderByStartDesc(Long userId);
+
+    @Query("SELECT new ru.practicum.shareit.item.model.Item(it.id, it.ownerId, it.name, it.description, it.available) "+
+            "FROM Item as it " +
+            "WHERE it.available = TRUE AND (" +
+            "lower(it.name)        LIKE lower(concat('%', :searchQuery, '%')) OR " +
+            "lower(it.description) LIKE lower(concat('%', :searchQuery, '%')) " +
+            ")"
+    )
+    List<Item> findAllAvailableAndContainingQueryIgnoreCase(
+            @Length(min = 1, message = "Запрос не может быть пустым") String searchQuery
+    );
+
+    @Query("SELECT new ru.practicum.shareit.booking.dto.BookingDto(b.id, b.itemId, b.start, b.end, b.bookerId) " +
+            "FROM Booking AS b " +
+            "WHERE b.itemId = :itemId and b.end = (" +
+                "SELECT MAX(b.end) " +
+                "FROM Booking AS b " +
+                "WHERE b.end < CURRENT_TIMESTAMP " +
+                "AND b.itemId = :itemId " +
+            ")")
+    BookingDto getLastBooking(Long itemId);
+
+    @Query("SELECT new ru.practicum.shareit.booking.dto.BookingDto(b.id, b.itemId, b.start, b.end, b.bookerId) " +
+            "FROM Booking AS b " +
+            "WHERE b.itemId = :itemId and b.start = (" +
+                "SELECT MIN(b.start) " +
+                "FROM Booking AS b " +
+                "WHERE b.start > CURRENT_TIMESTAMP " +
+                "AND b.itemId = :itemId " +
+                "AND b.status <> ru.practicum.shareit.booking.model.BookingStatus.REJECTED " +
+            ")")
+    BookingDto getNextBooking(Long itemId);
+
+}
---
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepositoryImpl.java
@@ -0,0 +1,103 @@
+package ru.practicum.shareit.booking.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingState;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class BookingRepositoryImpl implements BookingRepositoryCustom{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public BookingRepositoryImpl(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    private enum SearchBy {
+        USER_ITEMS,
+        USER_BOOKINGS
+    }
+
+    @Override
+    public List<Booking> findAllByUserItemsAndFilterByState(
+            Long userId,
+            BookingState state
+    ) {
+        return findAndFilter(userId, state, SearchBy.USER_ITEMS);
+    }
+
+    @Override
+    public List<Booking> findAllByUserBookingsAndFilterByState(
+            Long userId,
+            BookingState state
+    ) {
+        return findAndFilter(userId, state, SearchBy.USER_BOOKINGS);
+    }
+
+
+    private List<Booking> findAndFilter(Long userId, BookingState state, SearchBy searchBy) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Booking> cq = cb.createQuery(Booking.class);
+
+        Root<Booking> root = cq.from(Booking.class);
+        List<Predicate> predicates = mapStateToPredicates(cb, root, state);
+
+        if (searchBy.equals(SearchBy.USER_BOOKINGS)) {
+            predicates.add(cb.equal(root.get("bookerId"), userId));
+        } else {
+            //В дальнейшем костыль можно будет заменить на Join
+            List<Long> userItems =
+                    itemRepository.findAllByOwnerIdOrderByIdAsc(userId).stream().map(Item::getId).collect(Collectors.toList());
+            predicates.add(root.get("itemId").in(userItems));
+        }
+
+        cq.select(root)
+                .where(predicates.toArray(new Predicate[]{}))
+                .orderBy(cb.desc(root.get("start")))
+        ;
+        return entityManager.createQuery(cq).getResultList();
+    }
+
+    private List<Predicate> mapStateToPredicates(CriteriaBuilder cb, Root<Booking> root, BookingState state) {
+        List<Predicate> predicates = new ArrayList<>();
+        LocalDateTime now = LocalDateTime.now();
+
+        switch (state) {
+            case ALL:
+                break;
+            case CURRENT:
+                predicates.add(cb.lessThan(root.get("start"), now));
+                predicates.add(cb.greaterThan(root.get("end"), now));
+                //predicates.add(cb.equal(root.get("status"), BookingStatus.APPROVED));
+                break;
+            case PAST:
+                predicates.add(cb.lessThan(root.get("end"), now));
+                break;
+            case FUTURE:
+                predicates.add(cb.greaterThan(root.get("start"), now));
+                break;
+            case WAITING:
+                predicates.add(cb.equal(root.get("status"), BookingStatus.WAITING));
+                break;
+            case REJECTED:
+                predicates.add(cb.equal(root.get("status"), BookingStatus.REJECTED));
+                break;
+        }
+        return predicates;
+    }
+
+}
