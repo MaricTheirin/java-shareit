d1a9fcf6f30132dd649532bc8ca41400b6f20c61
MaricTheirin <ghostmeee@yandex.ru>
2023-03-29 02:34:13 +0300

    chore: Убрать неиспользуемые импорты

--- src/main/java/ru/practicum/shareit/item/mapper/CommentDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentDtoMapper.java
@@ -1,14 +1,12 @@
 package ru.practicum.shareit.item.mapper;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.CommentResponseDto;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.UserRepository;
 import java.time.LocalDateTime;
 
 @Slf4j
