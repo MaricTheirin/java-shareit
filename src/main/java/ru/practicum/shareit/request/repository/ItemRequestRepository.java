0ee8cd243734853719d66f8215abf056d62da826
MaricTheirin <ghostmeee@yandex.ru>
2023-04-02 02:41:16 +0300

    chore: Оптимизировать код, убрать лишние проверки

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.request.model.ItemRequest;
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
 
-    List<ItemRequest> findAllByUserIdOrderByCreatedDesc(Long userId);
+    Optional<List<ItemRequest>> findAllByUserIdOrderByCreatedDesc(Long userId);
 
-    List<ItemRequest> findAllByUserIdNotOrderByCreatedDesc(Long userId, Pageable pageable);
+    Optional<List<ItemRequest>> findAllByUserIdNotOrderByCreatedDesc(Long userId, Pageable pageable);
 
 }
