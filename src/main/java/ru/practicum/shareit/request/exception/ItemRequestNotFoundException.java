5efdc6ffb533b075d2c9658c5a59cfe5cb7062de
MaricTheirin <ghostmeee@yandex.ru>
2023-04-02 02:40:29 +0300

    feat: Добавить реализацию NotFoundException со стандартным сообщением

--- src/main/java/ru/practicum/shareit/request/exception/ItemRequestNotFoundException.java
+++ src/main/java/ru/practicum/shareit/request/exception/ItemRequestNotFoundException.java
@@ -1,10 +1,14 @@
 package ru.practicum.shareit.request.exception;
 
-import org.springframework.http.HttpStatus;
-import ru.practicum.shareit.service.exception.ShareItException;
+import ru.practicum.shareit.service.exception.NotFoundException;
 
-public class ItemRequestNotFoundException extends ShareItException {
+public class ItemRequestNotFoundException extends NotFoundException {
+
     public ItemRequestNotFoundException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
+        super(message);
+    }
+
+    public ItemRequestNotFoundException() {
+        super("Запрос на бронь не найден");
     }
 }
