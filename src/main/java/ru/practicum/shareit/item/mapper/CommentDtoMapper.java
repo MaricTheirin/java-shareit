fe0023e3ca119cc4275fbaca6893bd31608884fb
MaricTheirin <ghostmeee@yandex.ru>
2023-04-04 14:00:38 +0300

    chore: Изменить уровень видимости конструктора мапперов

--- src/main/java/ru/practicum/shareit/item/mapper/CommentDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentDtoMapper.java
@@ -1,7 +1,8 @@
 package ru.practicum.shareit.item.mapper;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.CommentResponseDto;
 import ru.practicum.shareit.item.model.Comment;
@@ -10,7 +11,7 @@ import ru.practicum.shareit.user.model.User;
 import java.time.LocalDateTime;
 
 @Slf4j
-@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentDtoMapper {
 
     private static final String OBJECT_MAPPED_MESSAGE = "Выполнено преобразование объекта из {} в {}";
