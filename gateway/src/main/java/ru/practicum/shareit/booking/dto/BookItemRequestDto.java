7eb8dd2975d6e85eb24a8ea08ac5709b56d7e8bb
MaricTheirin <ghostmeee@yandex.ru>
2023-04-06 11:28:39 +0300

    feat: Добавить расширенную валидацию для BookItemRequestDto

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,21 +1,26 @@
 package ru.practicum.shareit.booking.dto;
 
 import java.time.LocalDateTime;
-
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
-
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import ru.practicum.shareit.service.validation.Create;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class BookItemRequestDto {
-	private long itemId;
-	@FutureOrPresent
-	private LocalDateTime start;
-	@Future
-	private LocalDateTime end;
+
+	@NotNull(groups = Create.class, message = "Необходимо указать ID предмета")
+	private final Long itemId;
+
+	@NotNull(groups = Create.class, message = "Невозможно создать бронирование без даты начала")
+	@FutureOrPresent(groups = Create.class, message = "Начало бронирования не может быть в прошлом")
+	private final LocalDateTime start;
+
+	@NotNull(groups = Create.class, message = "Невозможно создать бронирование без даты окончания")
+	@Future(groups = Create.class, message = "Окончание бронирования не может быть в прошлом")
+	private final LocalDateTime end;
+
 }
