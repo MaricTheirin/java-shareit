ab15a9889f146fda5c195f8152d1e7ad8f52646a
MaricTheirin <ghostmeee@yandex.ru>
2023-04-08 09:50:17 +0300

    chore: Заменить ```@RestController``` на ```@Controller```

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -3,13 +3,14 @@ package ru.practicum.shareit.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.service.validation.Create;
 import ru.practicum.shareit.service.validation.Update;
 import ru.practicum.shareit.user.dto.UserDto;
 
-@RestController
+@Controller
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
 @Slf4j
